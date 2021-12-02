45b8bac3fe73699c180c401fe69ac3fa4ebdd4cb
Siromolot <siromolot.it@gmail.com>
2021-12-02 15:48:24 +0300

    Style: modal window width

--- frontend/src/actions/day.js
+++ frontend/src/actions/day.js
@@ -4,6 +4,12 @@ import {
 	GET_DAY_FAILED,
 } from '../constants';
 
-export const getDay = (data) => async (dispatch) => {
-
+export const getDay = (data) => async (dispatch, getStore) => {
+	const store = getStore();
+	const sendingData = {
+		day: data,
+		department: store.department.department
+	}
+	
+	console.log('sendingData: ', sendingData);
 }
